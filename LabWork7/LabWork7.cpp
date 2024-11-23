3ea6a39b205ce19e270fef9cc8df2dd8ad7af4fb
undervalued1 <157449229+undervalued1@users.noreply.github.com>
2024-11-23 10:33:33 +0500

    Add files via upload

---
+++ LabWork7/LabWork7.cpp
@@ -0,0 +1,29 @@
+﻿#include <iostream>
+using namespace std;
+void RevPrint(int N) {
+    if (N == 0) return;
+    cout << N % 10;
+    RevPrint(N / 10);
+}
+int main()
+{
+    setlocale(LC_ALL, "RU"); setlocale(LC_ALL, "RU");
+    cout << "Введите число >0: ";
+    int n;
+    cin >> n;
+    if (n < 0)
+    {
+        cout << "Введите не отрицательное число";
+        return 0;
+    }
+    if (n == 0){
+        cout << "Нужно вводить число >0";
+    }
+    else {
+        cout << "Число в обратном порядке: ";
+        RevPrint(n);
+    }
+    cout << endl;
+    return 0;
+}
+
